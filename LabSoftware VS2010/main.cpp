e5c0efb9fa4ca95c6b62e9eca48699634f267863
mitchwenman <mitchwenman@gmail.com>
2015-03-02 19:00:41 +1000

    first commit

---
+++ LabSoftware VS2010/main.cpp
@@ -0,0 +1,227 @@
+#include <stdlib.h>			//- for exit()
+#include <stdio.h>			//- for sprintf()
+#include <string.h>			//- for memset()
+
+#ifdef _WIN32
+	#include "libs/glut.h"
+	#include <windows.h>
+	#pragma comment(lib, "winmm.lib")			//- not required but have it in anyway
+	#pragma comment(lib, "libs/glut32.lib")
+#elif __APPLE__
+	#include <GLUT/glut.h>
+#elif __unix__		// all unices including  __linux__
+	#include <GL/glut.h>
+#endif
+
+//====== Macros and Defines =========
+
+#define FRAME_WIDE	1000
+#define FRAME_HIGH	600
+
+//====== Structs & typedefs =========
+typedef unsigned char BYTE;
+struct POINT2D {int x, y;};
+
+//====== Global Variables ==========
+BYTE	pFrameL[FRAME_WIDE * FRAME_HIGH * 3];
+BYTE	pFrameR[FRAME_WIDE * FRAME_HIGH * 3];
+int		shade = 0;
+POINT2D	xypos = {0,0};
+int		stereo = 0;
+int		eyes = 10;
+
+//===== Forward Declarations ========
+void ClearScreen();
+void DrawFrame();
+void Interlace(BYTE* pL, BYTE* pR);
+void PlaySoundEffect(char * filename);
+void BuildFrame(BYTE *pFrame, int view);
+void OnIdle(void);
+void OnDisplay(void);
+void reshape(int w, int h);
+void OnMouse(int button, int state, int x, int y);
+void OnKeypress(unsigned char key, int x, int y);
+
+////////////////////////////////////////////////////////
+// Program Entry Poinr
+////////////////////////////////////////////////////////
+
+int main(int argc, char** argv)
+{
+	//-- setup GLUT --
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);	//GLUT_3_2_CORE_PROFILE |
+	glutInitWindowSize(FRAME_WIDE, FRAME_HIGH);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+/*
+#ifdef WIN32
+	//- eliminate flashing --
+	typedef void (APIENTRY * PFNWGLEXTSWAPCONTROLPROC) (int i);
+	PFNWGLEXTSWAPCONTROLPROC wglSwapControl = NULL;
+	wglSwapControl = (PFNWGLEXTSWAPCONTROLPROC) wglGetProcAddress("wglSwapIntervalEXT");
+	if (wglSwapControl != NULL) wglSwapControl(1); 
+#endif
+*/
+
+	//--- set openGL state --
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	//-- register call back functions --
+	glutIdleFunc(OnIdle);
+	glutDisplayFunc(OnDisplay);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(OnKeypress);
+	glutMouseFunc(OnMouse);
+
+	//-- run the program
+	glutMainLoop();
+	return 0;
+}
+
+
+////////////////////////////////////////////////////////
+// Event Handers
+////////////////////////////////////////////////////////
+      
+void OnIdle(void)
+{
+	DrawFrame();
+	glutPostRedisplay();
+}
+
+
+void OnDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glRasterPos2i(0, 0);
+	glDrawPixels(FRAME_WIDE, FRAME_HIGH, GL_RGB,GL_UNSIGNED_BYTE, (GLubyte*)pFrameR);
+	glutSwapBuffers();
+	glFlush();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, (GLdouble) w, 0.0, (GLdouble) h);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+
+void OnMouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
+	{
+		PlaySoundEffect("Laser.wav"); 
+		if (++shade > 16) shade = 0;	
+	}
+}
+
+void OnKeypress(unsigned char key, int x, int y)
+{
+	switch (key) 
+	{ 
+	case ' ': xypos.x = xypos.y = 0; break;
+	case 's': stereo ^= 1, eyes = 10;break;
+	case ']': eyes++;	break;
+	case '[': eyes--;	break;
+	case 27 : exit(0);
+	}
+	PlaySoundEffect("Whoosh.wav"); 
+}
+
+
+////////////////////////////////////////////////////////
+// Utility Functions
+////////////////////////////////////////////////////////
+
+
+void ClearScreen()
+{
+	memset(pFrameL, 0, FRAME_WIDE * FRAME_HIGH * 3);
+	memset(pFrameR, 0, FRAME_WIDE * FRAME_HIGH * 3);
+}
+
+
+void Interlace(BYTE* pL, BYTE* pR)
+{
+	int rowlen = 3 * FRAME_WIDE;
+	for (int y = 0; y < FRAME_HIGH; y+=2)
+	{
+		for (int x = 0; x < rowlen; x++) *pR++ = *pL++;
+		pL += rowlen;
+		pR += rowlen;
+	}
+}
+
+
+void DrawFrame()
+{
+	ClearScreen();
+	
+	if (!stereo) BuildFrame(pFrameR, 0);
+	else {
+		BuildFrame(pFrameL, -eyes);
+		BuildFrame(pFrameR, +eyes);
+		Interlace((BYTE*)pFrameL, (BYTE*)pFrameR);
+	}
+}
+
+void	PlaySoundEffect(char * filename)		
+{
+#ifdef _WIN32
+	PlaySound(filename, NULL, SND_ASYNC | SND_FILENAME ); 
+#else
+	char command[80];
+	#ifdef __APPLE__
+		sprintf(command, "afplay %s &", filename);
+	#else
+		sprintf(command, "play %s &", filename);
+	#endif	
+	system(command);
+#endif
+}
+
+////////////////////////////////////////////////////////
+// Drawing Function
+////////////////////////////////////////////////////////
+
+void BuildFrame(BYTE *pFrame, int view)
+{
+	BYTE*	screen = (BYTE*)pFrame;		// use copy of screen pointer for safety
+
+	int		SBox = 250;					// size of box in pixels
+	int		channels = 3;				// number of colour channels
+
+	////////////////////////////////////////////
+	// Display some animated shaded boxes - hacky method
+	////////////////////////////////////////////
+
+	for (int x= xypos.x; x < xypos.x+SBox ; x++)
+		for (int y= xypos.y; y <xypos.y+SBox ; y++)
+		{
+			screen[channels*(x + view + y * FRAME_WIDE) + shade] = y-xypos.y;		// for 24 bit screen
+		}
+
+	if (++xypos.x +SBox  + view >= FRAME_WIDE ) xypos.x = 0;		// do not write past screen X boundary
+	if (++xypos.y +SBox >= FRAME_HIGH ) xypos.y = 0;		// do not write past screen Y boundary
+
+		//xypos.x = 200;
+		//xypos.y = 200;
+	//-- get rid of horizontal background edge --
+	if (xypos.y) for (int x= xypos.x; x < xypos.x+SBox ; x++)
+	{
+		screen[channels*(x + view + (xypos.y-1) * FRAME_WIDE) + shade] = 0;
+	}
+
+	//-- get rid of vertical background edge --
+	if (xypos.x) for (int y= xypos.y; y <xypos.y+SBox ; y++)
+	{
+		screen[channels*(view + xypos.x-1 + y * FRAME_WIDE) + shade] = 0;
+	}
+}
