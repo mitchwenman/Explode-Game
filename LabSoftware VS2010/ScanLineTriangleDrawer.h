3d333b62d590acadedf5a1250a79aa203a4f2039
mitchwenman <mitchwenman@gmail.com>
2015-03-10 08:45:36 +1000

    added sltri drawer header

---
+++ LabSoftware VS2010/ScanLineTriangleDrawer.h
@@ -0,0 +1,12 @@
+#ifndef SCANLINETRIANGLEDRAWER_
+#define SCANLINETRIANGLEDRAWER_
+
+#include "ScanLineTriangle.h"
+
+namespace ScanLineTriangleDrawer
+{
+	void draw(ScanLineTriangle* tri);
+}
+
+
+#endif
