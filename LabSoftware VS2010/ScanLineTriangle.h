adf12fdf4f184e6a6e2bb139f152b989f109c0e7
mitchwenman <mitchwenman@gmail.com>
2015-03-09 17:52:35 +1000

    added ScanLineTriangle header and cpp file

---
+++ LabSoftware VS2010/ScanLineTriangle.h
@@ -0,0 +1,16 @@
+#include "RGBColor.h"
+
+class ScanLineTriangle
+{
+
+public:
+	int XTopVert, YTopVert, XLeftVert, YLeftVert, XRightVert, YRightVert;
+	int hRight, hLeft;
+	RGBColour* colourTop, *colourLeft, *colourRight; 
+
+	ScanLineTriangle(int x1, int y1, int x2, int y2, int x3, int y3, 
+						RGBColour* colour1, RGBColour* colour2, RGBColour* colour3);
+
+private:
+	void calculateScanLineTriangle();
+};
---
+++ LabSoftware VS2010/ScanlineTriangle.cpp
@@ -0,0 +1,13 @@
+#include "ScanLineTriangle.h"
+
+
+ScanLineTriangle::ScanLineTriangle(int x1, int y1, int x2, int y2, int x3, int y3, 
+						RGBColour* colour1, RGBColour* colour2, RGBColour* colour3)
+{
+
+}
+
+void ScanLineTriangle::calculateScanLineTriangle()
+{
+
+}
