bb0cd3f249930451174a13985a747395b8e2ca69
mitchwenman <mitchwenman@gmail.com>
2015-03-14 08:46:50 +1000

    polygon2d constructor with vertex arguments

--- LabSoftware VS2010/Polygon2D.cpp
+++ LabSoftware VS2010/Polygon2D.cpp
@@ -4,3 +4,12 @@ Polygon2D::Polygon2D(int numSides, GPLine* sides[]) : numSides(numSides)
 {
 	this->sides.assign(sides, sides + numSides);
 }
+
+Polygon2D::Polygon2D(int numVerts, POINT2D verts[])
+{
+	for (int i = 0; i < numVerts - 2; i++)
+	{
+		this->sides.push_back(new GPLine(verts[i].x, verts[i].y, verts[i + 1].x, verts[i + 1].y));
+	}
+	this->numSides = numVerts;
+}
--- LabSoftware VS2010/Polygon2D.h
+++ LabSoftware VS2010/Polygon2D.h
@@ -2,6 +2,7 @@
 #define POLYGON2D_
 
 #include "GPLine.h"
+#include "GraphicsUtil.h"
 #include <vector>
 
 class Polygon2D
@@ -13,6 +14,7 @@ public:
 
 	Polygon2D() {};
 	Polygon2D(int numSides, GPLine* sides[]);
+	Polygon2D(int numVerts, POINT2D verts[]);
 };
 
 
