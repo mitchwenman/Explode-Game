4c9d05530c77a4cf0b619041bdceee33cef6ac5b
mitchwenman <mitchwenman@gmail.com>
2015-04-13 15:12:50 +1000

    renamed vjs files

--- LabSoftware VS2010/VJSReader.h
+++ LabSoftware VS2010/VJSReader.h
@@ -2,11 +2,16 @@
 #define VJSREADER_
 
 #include "Polygon3D.h"
+#include <vector>
 
 namespace VJSReader
 {
 	//Reads a VJS file and creates a polygon from it
 	Polygon3D* read(std::string fileName);
+
+	//Reads a VJS index file and returns a vector of filenames
+	std::vector<std::string> readIndexFile(std::string filename);
+
 }
 
 #endif
