21524e036da5b516f371f09ac5a6f1fb61a7275a
mitchwenman <mitchwenman@gmail.com>
2015-04-14 14:40:24 +1000

    missed polygon status item

--- LabSoftware VS2010/MissedPolygonStatusItem.cpp
+++ LabSoftware VS2010/MissedPolygonStatusItem.cpp
@@ -1,10 +1,19 @@
 #include "MissedPolygonStatusItem.h"
 #include "Game.h"
-
+#include "GraphicsUtil.h"
+#include "GraphicsSettings.h"
+#include "libs/glut.h"
+#include <sstream>
 
 void MissedPolygonStatusItem::draw()
 {
 	Game* game = Game::getSingleton();
 	int numMissedPolygons =  game->getNumberOfMissedPolygons();
-	
+	int maxMissedPolygons = game->MAX_MISSED_POLYGONS;
+	GraphicsSettings *gset = GraphicsSettings::getGraphicsSettings();
+	std::stringstream stream;
+	stream << "Missed: " << numMissedPolygons << " / " << maxMissedPolygons;
+	GraphicsUtil::drawString(GLUT_BITMAP_TIMES_ROMAN_24, stream.str(),
+		gset->getFrameWidth() - MPSI_XOFFSET_RIGHT, MPSI_YOFFSET_BOTTOM);
+				
 }
--- LabSoftware VS2010/MissedPolygonStatusItem.h
+++ LabSoftware VS2010/MissedPolygonStatusItem.h
@@ -2,6 +2,10 @@
 
 class MissedPolygonStatusItem : public IStatusItem
 {
+private:
+	static const int MPSI_XOFFSET_RIGHT = 500;
+	static const int MPSI_YOFFSET_BOTTOM = 25;
+
 public:
 	void draw();
 
