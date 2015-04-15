cd5be625335c3db4967b9bb08285ea5131c22501
Mitch Wenman <mitchwenman@gmail.com>
2015-04-15 12:51:26 +1000

    possible fix for linker error

--- LabSoftware VS2010/GraphicsUtil.h
+++ LabSoftware VS2010/GraphicsUtil.h
@@ -4,14 +4,6 @@
 #include <string>
 
 /* A utility file for random but useful functions/types */
-
-namespace GraphicsUtil
-{
-	//Draws a string to the frame
-	void drawString(void *font, std::string str, int x, int y);
-}
-
-
 #define ROUND(x) ((int)(x + 0.5))
 
 typedef struct  {int x, y;} POINT2D;
@@ -21,6 +13,15 @@ typedef struct { int x; int y; RGBColour c; } VERTEX;
 typedef struct { double x; double y; double z; RGBColour c; } VERTEX_3D;
 
 typedef struct { double x; double y; double z; } VERTEX_3D_f;
+
+namespace GraphicsUtil
+{
+	//Draws a string to the frame
+	void drawString(void *font, std::string str, int x, int y);
+}
+
+
+
 
 
 
